670f0ee855543176237c6d2f270ae3aaabb02aee
janani <chravija@gmail.com>
2025-02-17 08:52:27 +0530

    day-6 task

---
+++ javascript/function.js
@@ -0,0 +1,20 @@
+function add(a,b)
+{
+return a+b;
+}
+function sub(a,b)
+{
+    return a-b;
+}
+function multiply(a,b)
+{
+    return a*b;
+}
+function print()
+{
+    return "hello";
+}
+console.log(add(4,19));
+console.log(sub(9,3));
+console.log(multiply(8,9));
+console.log(print());
