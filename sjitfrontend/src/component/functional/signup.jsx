48559b990daa19ec2eea920dad844f24d4c38e04
janani <chravija@gmail.com>
2025-02-21 14:37:32 +0530

    task

--- sjitfrontend/src/component/functional/signup.jsx
+++ sjitfrontend/src/component/functional/signup.jsx
@@ -2,10 +2,37 @@ function Signup() {
     return (
         <div style={{ textAlign: "center", marginTop: "20vh" }}>
             <h1>Signup</h1>
+            <p>Enter your details to create an account</p>
             <form>
-                <input type="email" placeholder="Email" required /><br /><br />
-                <input type="password" placeholder="Password" required /><br /><br />
-                <button type="submit">Signup</button>
+                <input 
+                    type="email" 
+                    placeholder="Email" 
+                    required 
+                    style={{ padding: "8px", width: "250px" }}
+                />
+                <br /><br />
+
+                <input 
+                    type="password" 
+                    placeholder="Password" 
+                    required 
+                    style={{ padding: "8px", width: "250px" }}
+                />
+                <br /><br />
+
+                <button 
+                    type="submit" 
+                    style={{ 
+                        padding: "8px 16px", 
+                        backgroundColor: "#28a745", 
+                        color: "white", 
+                        border: "none", 
+                        cursor: "pointer",
+                        borderRadius: "4px"
+                    }}
+                >
+                    Signup
+                </button>
             </form>
         </div>
     );
